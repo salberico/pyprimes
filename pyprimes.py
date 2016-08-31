5361c88addad9d332902f2bd7ddcae9a468479ed
ADMIN <sam.alberico@gmail.com>
2016-08-30 22:12:35 -0400

    added sieve and gcd algorithms

--- pyprimes.py
+++ pyprimes.py
@@ -9,22 +9,22 @@ def isprime(n):
 		return True
 	if n % 2 == 0 or n % 3 == 0 or n == 1:
 		return False
-		
+
 	fac = 5
 	inc = 2
-	
+
 	while fac * fac <= n:
 		if n % fac == 0:
-			return False	
-			
+			return False
+
 		# increment is either 4 or 2 therefore holding the for 6i +- 1 for increment
 		fac += inc
-		
+
 		# Flip flop increment from 4 to 2
 		inc = 6 - inc
 
 	return True
-	
+
 def factorize(n):
 	"""
 	Basic prime factorization with small optimizations.
@@ -37,21 +37,21 @@ def factorize(n):
 			if x % 2 == 0:
 				out.append(2), reduce(x//2)
 				return
-				
+
 			# Check odd factors
 			for factor in range(3,int(x**0.5+1),2):
 				if x % factor == 0:
 					# Further factor the results
 					reduce(factor), reduce(x//factor)
-					return	
-					
+					return
+
 		# Will be prime if this is reached
 		out.append(x)
-		
+
 	# Start recursion
 	reduce(n)
 	return out
-	
+
 def miller_rabin(n):
 	"""
 	Basic miller rabin primality test:
@@ -89,6 +89,52 @@ def miller_rabin(n):
 		else:
 			return False
 	return True
+
+def gcd(x, y):
+	"""
+	Recursive Euclidean Algorithm, for calculating 'Greatest Common Denominator'(GCD)
+	"""
+	if not x:
+		return y
+	else:
+		return gcd(x, y%x)
+
+def sieve(n):
+	"""
+	Memory inefficient simple sieve of eratosthenes for prime generation up to n
+	still decently quick but can still be optimized greatly.
+	"""
+	
+	# Edge case handling
+	if n < 2:
+		return []
+	if n == 2:
+		return [2]
+		
+	# Initialize base number list
+	numbers = [2] + list(range(3,n+1,2))
+	index = 1
+	increment = numbers[index]
+	
+	# Store calculations which are reused
+	t_count = len(numbers)
+	t_root = n**0.5+1
+	
+	while increment < t_root:
+		# 'Removing' (making zero) numbers which are multiples of current increment
+		# since the list is in order just have to count by the said increment
+		for remove_index in range(index+increment, t_count, increment):
+			numbers[remove_index] = 0
+
+		# Get next non-zero increment, which will be a prime at this point
+		for candidate_index in range(index+1, t_count):
+			if numbers[candidate_index] != 0:
+				index = candidate_index
+				increment = numbers[index]
+				break
+				
+	# Return only the non-zero numbers which are proven primes
+	return [number for number in numbers if number != 0]
 	
 def spiral(layers):
 	"""
@@ -99,30 +145,29 @@ def spiral(layers):
 
 	if layers <= 0:
 		return []
-		
+
 	# Get sequence of indices which are to be used at the said layer offset from the center
 	def getsequence(L):
-		return np.concatenate((np.arange(L), np.ones(2*L+1, dtype=np.int64)*L, 
-			np.arange(L-1,-L,-1), np.ones(2*L+1, dtype=np.int64)*-L, np.arange(1-L,0)))    
-			
+		return np.concatenate((np.arange(L), np.ones(2*L+1, dtype=np.int64)*L,
+			np.arange(L-1,-L,-1), np.ones(2*L+1, dtype=np.int64)*-L, np.arange(1-L,0)))
+
 	count = 1
 	arr = np.ones((layers*2-1,layers*2-1), dtype=np.uint32)
 	for layer in range(1, layers):
-	
+
 		# Generate the Y indices for the layer and offset based on the center coordinates
 		sequenceY = getsequence(layer) + layers - 1
-		
+
 		# Offset X indices from the generated Y
 		sequenceX = np.roll(sequenceY, -2*layer)
-		
+
 		# Assign incremental values to the indices in order
 		for x, y in zip(sequenceX, sequenceY):
 			count += 1
 			arr[x,y] = count
-			
+
 	return arr
 
-	
 if __name__ == '__main__':
 	# Testing
 	import matplotlib.pyplot as plt
@@ -133,4 +178,3 @@ if __name__ == '__main__':
 			a[x,y] = isprime(a[x,y])
 	plt.figimage(a*255)
 	plt.show()
-
